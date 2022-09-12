2ab6a0792d02bd9b7ed74c043609ad286e56c42e
aedipi <ahmetemredipi@gmail.com>
2022-09-12 20:58:16 +0300

    Initial commit

---
+++ lib/entity/kisiler.dart
@@ -0,0 +1,16 @@
+class Kisiler {
+  String kisi_id;
+  String kisi_ad;
+  String kisi_tel;
+
+  Kisiler({required this.kisi_id,required this.kisi_ad,required this.kisi_tel});
+
+  factory Kisiler.fromJson(String key,Map<dynamic,dynamic> json){
+    //firebase özelinde keyde dynamic olabiliyor
+    return Kisiler(
+        kisi_id: key,
+        kisi_ad: json["kisi_ad"] as String,
+        kisi_tel: json["kisi_tel"] as String
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kisiler_uygulamasi/cubit/anasayfa_cubit.dart';
+import 'package:kisiler_uygulamasi/cubit/kisi_detay_cubit.dart';
+import 'package:kisiler_uygulamasi/cubit/kisi_kayit_cubit.dart';
+import 'package:kisiler_uygulamasi/views/anasayfa.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => KisiKayitCubit()),
+        BlocProvider(create: (context) => KisiDetayCubit()),
+        BlocProvider(create: (context) => AnasayfaCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Anasayfa(),
+      ),
+    );
+  }
+}
+
